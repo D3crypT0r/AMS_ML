d6e265295c3e3971708e4e93544216a0d33d2501
(Syed Rizwan Hilal Shah) <66831571+D3crypT0r@users.noreply.github.com>
2024-10-24 17:06:47 +0530

    Update attendance_logger.py

--- attendance_logger.py
+++ attendance_logger.py
@@ -14,7 +14,6 @@ def log_attendance(student_id, student_name):
 
         df = pd.DataFrame(data)
 
-        # Append to the existing Excel file or create a new one if it doesn't exist
         file_name = 'attendance.xlsx'
         with pd.ExcelWriter(file_name, mode='a' if pd.io.common.file_exists(file_name) else 'w', engine='openpyxl') as writer:
             df.to_excel(writer, index=False, header=not writer.sheets)
